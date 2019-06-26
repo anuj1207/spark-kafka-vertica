fbc9272e51b9c0d78ae2c0b7c413bc5093666222
Anuj Saxena <anuj.633@gmail.com>
2019-06-26 06:48:17 +0530

    Add application for reading data from Vertica

--- src/main/scala/edu/technohub/com/app/streaming/structured/StructuredStreamingKafkaToVerticaApplication.scala
+++ src/main/scala/edu/technohub/com/app/streaming/structured/StructuredStreamingKafkaToVerticaApplication.scala
@@ -1,20 +1,20 @@
-package edu.technohub.com
+package edu.technohub.com.app.streaming.structured
 
 import edu.technohub.com.models.Person
 import edu.technohub.com.services.SparkService
 import edu.technohub.com.sources.{KafkaSource, VerticaSource}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-object StructuredStreamingApplication extends SparkService(new KafkaSource, new VerticaSource) with App{
+object StructuredStreamingKafkaToVerticaApplication extends SparkService(new KafkaSource, new VerticaSource) with App{
 
   val spark: SparkSession = createSparkSession()
 
   val df: DataFrame = createStreamingDataFrame(spark)
 
-  val parsedDataFrame: DataFrame = df.transform(parseDataFrame(_, Person.schema)) //TODO::check::Use your model here
+  val parsedDataFrame: DataFrame = df.transform(parseFromOneColumn(_, Person.schema)) //TODO::check::Use your model here
 
   val transformedDF: DataFrame = parsedDataFrame.transform(transformationLogic) //TODO::check::Add you logic inside `transformationLogic`
 
-  writeStreamingDF(transformedDF).awaitTermination()
+  customWriteStreamingDF(transformedDF).awaitTermination()
 
 }
--- src/main/scala/edu/technohub/com/services/SparkService.scala
+++ src/main/scala/edu/technohub/com/services/SparkService.scala
@@ -7,12 +7,13 @@ import edu.technohub.com.utils.Logging
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.{OutputMode, StreamingQuery, Trigger}
+import org.apache.spark.sql.streaming.OutputMode.Append
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import scala.concurrent.duration._
 
-class SparkService(inbound: Source, outbound: Source) extends Logging {
+class SparkService(val inbound: Source, val outbound: Source) extends Logging {
 
   val master: String = SparkConfig.getValueOpt("master").getOrElse{noConfigFound("master");SPARK_MASTER}
   val appName: String = SparkConfig.getValueOpt("app.name").getOrElse{noConfigFound("app.name");APP_NAME}
@@ -20,6 +21,10 @@ class SparkService(inbound: Source, outbound: Source) extends Logging {
   val checkpointLocation: String = SparkConfig.getValueOpt("checkpoint.location").getOrElse{noConfigFound("checkpoint.location");CHECKPOINT_PATH}
   val triggeringTime: Duration = SparkConfig.getLongOpt("trigger.time").getOrElse{noConfigFound("trigger.time");TRIGGERING_TIME}.seconds
 
+  /**
+    * Creates an object of SparkSession with given config
+    * @param config The configuration with which SparkSession needs to be created
+    * */
   def createSparkSession(config: Map[String, String] = Map.empty): SparkSession = {
 
     val spark = SparkSession.builder()
@@ -32,25 +37,71 @@ class SparkService(inbound: Source, outbound: Source) extends Logging {
     spark
   }
 
+  /**
+    * Creates a streaming DataFrame with given data source with the help of SparkSession
+    * @param sparkSession The object of SparkSession or the master
+    * @param opts         Properties to create a connection with the source to read data
+    * @param format       The format of source with which data is to be read
+    * @return             DataFrame with reference to the data
+    */
   def createStreamingDataFrame(sparkSession: SparkSession, opts: Map[String, String] = inbound.sourceOptions, format: String = inbound.dataSource): DataFrame =
     sparkSession.readStream.format(format).options(opts).load()
 
-  /**To parse DataFrame into a specified schema*/
-  def parseDataFrame(dataFrame: DataFrame, schema: StructType): DataFrame =
-    dataFrame.selectExpr(s"CAST($VALUE AS STRING)")
-      .select(from_json(col(VALUE), schema).as(VALUE))
-      .select(col(s"$VALUE.*"))
+  /**
+    * This method is to write streaming data to those sinks which aren't yet supported by Structured Streaming (like Vertica)
+    * @param dataFrame  A streaming DataFrame is required otherwise this method will not work
+    * @return           A streaming query which will continue to run for each trigger/new data comes from input source till the time of termination
+    */
+  def customWriteStreamingDF(dataFrame: DataFrame): StreamingQuery =
+    dataFrame.writeStream.outputMode(OutputMode.Append)
+      .option("checkpointLocation", checkpointLocation)
+      //      .trigger(Trigger.ProcessingTime(triggeringTime)) //TODO:check::Enable this to enable triggering after a certain time
+      .foreachBatch((ds, _) => outbound.saveToSource(ds)).start()
 
-  /**Use your transform logic here for your DataFrame*/
+  /**
+    * This method is to write streaming data to those sinks which are supported by Structured Streaming (like Kafka)
+    * @param dataFrame  A streaming DataFrame is required otherwise this method will not work
+    * @param opts       Properties to create a connection with the source to write data
+    * @param format     The format of source where data is to be written
+    * @param mode       Mode of saving data (complete / append / update)
+    * @return           A streaming query which will continue to run for each trigger/new data comes from input source till the time of termination
+    */
+  def writeStreamingDF(dataFrame: DataFrame, opts: Map[String, String] = outbound.sourceOptions, format: String = outbound.dataSource, mode: OutputMode = Append): StreamingQuery =
+    dataFrame.writeStream.format(format)
+      .options(opts)
+      //      .trigger(Trigger.ProcessingTime(triggeringTime)) //TODO:check::Enable this to enable triggering after a certain time
+      .outputMode(mode).start()
+
+  /**
+    * To parse data of one column of specified DataFrame into a given schema
+    * @param dataFrame  DataFrame containing the data
+    * @param schema     The schema which will be the schema of output DataFrame
+    * @param columnName The column which needs to be parsed using the specified schema
+    * @return           The parsed DataFrame which will have the same columns as schema
+    */
+  def parseFromOneColumn(dataFrame: DataFrame, schema: StructType, columnName: String = VALUE): DataFrame =
+    dataFrame.selectExpr(s"CAST($columnName AS STRING)")
+      .select(from_json(col(columnName), schema).as(columnName))
+      .select(col(s"$columnName.*"))
+
+  /**
+    * Merging all values as JSON into one column value
+    * @param dataFrame        DataFrame to be worked upon
+    * @param columnNames      The columns which needs to be merged as JSON
+    * @param finalColumnName  The output column
+    * @return                 DataFrame which only has the finalColumn having all the data from given column merged as JSON
+    */
+  def mergeIntoOneColumn(dataFrame: DataFrame, columnNames: List[String], finalColumnName: String = VALUE): DataFrame = {
+    val columns = columnNames.map(col)
+    dataFrame.select(to_json(struct(columns: _*)).as(finalColumnName))
+  }
+
+  /**
+    * Write your transform logic here for your DataFrame
+    */
   val transformationLogic: DataFrame => DataFrame = { dataFrame =>
     //TODO:check::Add your logic here
     dataFrame
   }
-
-  def writeStreamingDF(dataFrame: DataFrame): StreamingQuery =
-    dataFrame.writeStream.outputMode(OutputMode.Append)
-      .option("checkpointLocation", checkpointLocation)
-//      .trigger(Trigger.ProcessingTime(triggeringTime)) //TODO:check::Enable this to enable triggering after a certain time
-      .foreachBatch((ds, _) => outbound.saveToSource(ds)).start()
 
 }
